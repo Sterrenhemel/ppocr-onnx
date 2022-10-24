ec8ff20105085a3a4fbb7b5e4da95669200ffbc1
big-thousand <202121120113@std.uestc.edu.cn>
2022-10-24 08:24:44 +0000

    use gpu

--- demo/demo.py
+++ demo/demo.py
@@ -8,17 +8,14 @@ from ppocronnx.utility import draw_ocr_box_result
 
 def main():
     text_sys = TextSystem()
-    print(text_sys.ocr_single_line(cv2.imread('test3.png')))
-    img = cv2.imread('test.png')
-    img = cv2.resize(img, (0, 0), fx=1.5, fy=1.5, interpolation=cv2.INTER_LINEAR)
+    img_test = cv2.imread('test-003.jpg')
+    print(text_sys.ocr_single_line(img_test))
+    img = cv2.resize(img_test, (0, 0), fx=1.5, fy=1.5, interpolation=cv2.INTER_LINEAR)
     res = text_sys.detect_and_ocr(img, box_thresh=0.5, unclip_ratio=2)
     for boxed_result in res:
         print("{}, {:.3f}".format(boxed_result.ocr_text, boxed_result.score))
 
-    draw_img = draw_ocr_box_result(img, res, 0.5, 'WeiRuanYaHei-1.ttf')
-    cv2.imshow('test', draw_img)
-    cv2.waitKey()
-
+    # draw_img = draw_ocr_box_result(img, res, 0.5, 'WeiRuanYaHei-1.ttf')
 
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
